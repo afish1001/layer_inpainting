2d43898006c36c1f3a922f2d769d19c83e5b6663
bingmang <ustbbingmang@foxmail.com>
2018-12-18 12:13:13 +0800

    [update] inpaint.py

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-# layer-inpainting
-
-## file list
+# layer_inpainting 
 
 ```
-result
-    - NS
-        - Pha1_00020_value
-            - images
-                - interval_1
-            - interval_1.mat
-            - interval_2.mat
-            ...
+Usage: python inpaint.py
 
+Example:
+    data/
+        - Pha1_00020_value/
+            - interval_1/
+                - 000.png
+                - 001.png
+                ...
+            - interval_2/
+            - interval_3/
+    result/
+        - NS/
+            - Pha1_00020_value/
+                - images/
+                    - interval_1/
+                        - 000.png
+                        - 001.png
+                        ...
+                    - interval_2/
+                    - interval_3/
+                - interval_1.mat
+                - interval_2.mat
+                - interval_3.mat
+                ...
 ```
--- layer_inpainting/inpaint.py
+++ layer_inpainting/inpaint.py
@@ -1,7 +1,34 @@
 """
 层间图像修复（传统方法）
+输入文件夹默认为 data
+输出文件夹默认为 result
 
-Usage: python inpaint.py --input datasets/imgs --mask datasets/masks --gap 2 --out results
+Usage: python inpaint.py
+
+Example:
+    data/
+        - Pha1_00020_value/
+            - interval_1/
+                - 000.png
+                - 001.png
+                ...
+            - interval_2/
+            - interval_3/
+    result/
+        - NS/
+            - Pha1_00020_value/
+                - images/
+                    - interval_1/
+                        - 000.png
+                        - 001.png
+                        ...
+                    - interval_2/
+                    - interval_3/
+                - interval_1.mat
+                - interval_2.mat
+                - interval_3.mat
+                ...
+
 
 """
 
@@ -11,13 +38,10 @@ import multiprocessing
 import cv2
 import numpy as np
 import scipy.io as io
-from skimage.measure import compare_mse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', required=True, type=str, help='The input image directory.')
-# parser.add_argument('--mask', required=True, type=str, help='The mask image directory.')
-parser.add_argument('--out', type=str, default='./out', help='The output directory.')
-parser.add_argument('--gap', required=True, type=int, help='The number of blank line between two lines.')
+parser.add_argument('--input', type=str, default='./data', help='The input image directory.')
+parser.add_argument('--out', type=str, default='./result', help='The output directory.')
 
 opt = parser.parse_args()
 if not opt.out.endswith('/'):
@@ -33,6 +57,9 @@ GAP_RADIUS_MAP = {
     6: 26,
 }
 
+MASK_VALUE = 125  # 图像中标记mask的值为125
+INTERVAL_LIST = [1, 2, 3, 4, 5, 6]
+
 
 def mkoutdir(path):
     try:
@@ -41,14 +68,33 @@ def mkoutdir(path):
         pass
 
 
-def layer_inpainting(img, mask, gap, mode='NS'):
-    radius = GAP_RADIUS_MAP[gap]
+def layer_inpainting(img, mask, interval, mode='NS'):
+    """
+    层间图像修复，修复用的参数从MAP中取
+    :param img: 输入的图像
+    :param mask: mask图像
+    :param interval: 层间间隔
+    :param mode: 图像恢复的算法，NS或TELEA
+    :return: 修复的图像
+    """
+    radius = GAP_RADIUS_MAP[interval]
     if mode == 'NS':
         return cv2.inpaint(img, mask, radius, cv2.INPAINT_NS)
     elif mode == 'TELEA':
         return cv2.inpaint(img, mask, radius, cv2.INPAINT_TELEA)
     else:
         raise TypeError('The inpaint method is not supported')
+
+
+def binaryzation_(img, threshold=125):
+    """
+    二值化图像，会直接修改原始数据，所以在函数名后加下划线
+    :param img: numpy.darray
+    :param threshold: number, 0~255
+    :return: None
+    """
+    img[img < threshold] = 0
+    img[img >= threshold] = 255
 
 
 def get_flist(path):
@@ -60,43 +106,56 @@ def get_flist(path):
     return flist
 
 
-def inpaint_worker(img):
-    name, ext = os.path.basename(img).split('.')
-    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-    # mask = cv2.imread(mask, cv2.IMREAD_GRAYSCALE)
-    _mask = np.zeros(img.shape, dtype=np.uint8)
-    _mask[img == 125] = 255
-    # _mask = cv2.cvtColor(_mask, cv2.COLOR_RGB2GRAY)
-    dst = layer_inpainting(img, _mask, opt.gap, mode='TELEA')
-    dst[dst < 125] = 0
-    dst[dst >= 125] = 255
-    cv2.imwrite(opt.out + '{}.{}'.format(name, ext), dst)
+def mat_inpaint_worker(pha_dir, interval, mode='TELEA'):
+    """
+    输入pha文件夹路径和 interval
+    生成 interval_2.mat 文件和 images/interval_2/000.png-399.png
+    :param pha_dir: string,     example: ./data/Pha1_00020_value
+    :param interval: number,    example: 2
+    :param mode: string,        example: 'NS' or 'TELEA'
+    :return: None
+    """
+    # 获取 pha_dir 路径的名字, 如：Pha1_00020_value
+    pha_name = os.path.basename(os.path.dirname(pha_dir))
+    # 获取 interval 路径的名字，如：interval_2
+    interval_name = 'interval_{}'.format(interval)
+    # 获取各种路径
+    input_folder = os.path.join(pha_dir, interval_name)
+    out_folder = os.path.join(opt.out, mode, pha_name)
+    img_folder = os.path.join(out_folder, 'images', interval_name)
 
+    mkoutdir(img_folder)
 
-def mat_inpaint_worker(flist, gap):
-    mode = 'TELEA'
-    mat = np.zeros((400, 400, 400), dtype=np.uint8)
+    # 读取 Pha1_00020_value/interval_2 下的所有文件路径
+    flist = get_flist(input_folder)
+
+    # 从第一张图片获取mask（同一个interval下使用的mask是一样的）
+    img = cv2.imread(flist[0], cv2.IMREAD_GRAYSCALE)
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    mask[img == MASK_VALUE] = 255
+
+    mat = []
+
+    # 遍历flist，修复每层图像并推入mat中
     for i, file in enumerate(flist):
-        name, ext = os.path.basename(file).split('.')
         img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-        _mask = np.zeros(img.shape, dtype=np.uint8)
-        _mask[img == 125] = 255
-        layer = layer_inpainting(img, _mask, opt.gap, mode=mode)
-        layer[layer < 125] = 0
-        layer[layer >= 125] = 255
-        mat[:, :, i] = layer
-    file_name = '{}_interval_{}'.format(mode, gap)
-    io.savemat(file_name + '.mat', {file_name: mat})
+        layer = layer_inpainting(img, mask, interval, mode=mode)
+        binaryzation_(layer)
+        cv2.imwrite(os.path.join(img_folder, os.path.basename(file)), layer)
+        mat.append(layer)
+
+    # 将mat保存为 result/Pha1_00020_value/interval_2.mat , key = interval_2
+    mat = np.array(mat)
+    io.savemat(os.path.join(out_folder, interval_name + '.mat'), {interval_name: mat})
 
 
 if __name__ == '__main__':
-    mkoutdir(opt.out)
-    img_list = get_flist(opt.input)
-    # mask_list = get_flist(opt.mask)
+    pha_list = os.listdir(opt.input)
 
     # 多线程
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-    for i in range(len(img_list)):
-        pool.apply_async(inpaint_worker, (img_list[i],))
+    for pha_dir in pha_list:
+        for interval in INTERVAL_LIST:
+            pool.apply_async(mat_inpaint_worker, (pha_dir, interval, 'TELEA'))
     pool.close()
     pool.join()
